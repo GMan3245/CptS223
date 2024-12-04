3b1718661e550ee7cf7272fb87413cfcb8b7098c
GMan3245 <121774362+GMan3245@users.noreply.github.com>
2024-12-04 00:02:56 -0800

    Update SeparateChaining.h
    Update SeperateChaining.h

--- SeparateChaining.h
+++ SeparateChaining.h
@@ -21,33 +21,71 @@ class ChainingHash
 
     bool contains( const HashedObj & x ) const
     {
-        // TODO: refer to Figure 5.9 in textbook
-        return false;
+        const auto & whichList = theLists[myhash( x )];
+        return std::find(begin(whichList),end(whichList), x) != end(whichList);
     }
 
     void makeEmpty( )
     {
-        // TODO: refer to Figure 5.9 in textbook
+        currentSize = 0;
+        for (auto& entryList : theLists) {
+            entryList.clear();  // Clear each list in the vector
+        }
     }
 
-    bool insert( const HashedObj & x )
+    bool insert( const HashedObj & x ) //L Value Insert
     {
-        // TODO: refer to Figure 5.10 in textbook
+        auto & whichList = theLists[myhash( x )]; //gets the list for the hash value of x
+
+        //checks if x is already in the list to avoid duplicates
+        if(std::find(begin(whichList),end(whichList), x)!= end(whichList)) {
+            return false; //x already exists, so no insertion is done
+        }
+        whichList.push_back( x ); //insert x at the end of the list
         // this "insert" function accepts Lvalues
-        return false;
+
+        //increments the currentSIze and checks if the rehashing is needed
+        if(++currentSize > theLists.size()) {
+            rehash();
+        }
+
+        return true;
     }
     
-    bool insert( HashedObj && x )
+    bool insert( HashedObj && x ) //R value insert
     {
-        // TODO: this "insert" function accepts *Rvalues*
-        // so needs to use "move" (slightly different from the above one)
-        return false;
+        auto & whichList = theLists[myhash( x )]; //gets the list for the hash value of x
+
+        //checks if x is already in the list to avoid duplicates
+        if(std::find(begin(whichList),end(whichList), x)!= end(whichList)) {
+            return false; //x already exists, so no insertion is done
+        }
+        whichList.push_back(std::move( x )); //insert x at the end of the list using the move
+        // this "insert" function accepts Lvalues
+
+        //increments the currentSIze and checks if the rehashing is needed
+        if(++currentSize > theLists.size()) {
+            rehash();
+        }
+
+        return true;
     }
 
     bool remove( const HashedObj & x )
     {
-        // TODO: refer to Figure 5.9 in textbook
-        return false;
+
+        auto & whichList = theLists[myhash( x )];//gets the list corresponding to xs hash
+
+        //find x in the target list
+        auto itr = find(begin(whichList), end(whichList), x);
+
+        if (itr == end(whichList))    //if x is not found, return false
+            return false;
+
+        whichList.erase(itr); //remove x from the list to decrease currentsize
+        --currentSize;
+
+        return true;
     }
 
     double readLoadFactor() 
@@ -66,24 +104,40 @@ class ChainingHash
     }
 
   private:
-    vector<list<HashedObj>> theLists;   // The array of Lists
+
+    //vector<list<HashedObj>> theLists;   // The array of Lists
+    std::vector<std::list<HashedObj>> theLists;
     int currentSize;
 
     void rehash( )
     {
-        // TODO: refer to Figure 5.22 in textbook
+        //store the old list array temporarily
+        auto oldLists = theLists;
+
+        //resize theLists to the next prime number thats roughly double the current size
+        theLists.resize(nextPrime(2*theLists.size()));
+
+        //clear new list
+        for (auto & list : theLists)
+            list.clear();
+
+        //resent the currentSize to 0 so it can reinsert elements
+        currentSize = 0;
+
+
+        for( auto & list : oldLists )
+            for (auto& x : list)
+                insert(std::move(x));
     }
 
-    size_t myhash( const HashedObj & x ) const
-    {
-        static hash<HashedObj> hf;
-        return hf( x ) % theLists.size( );
+    size_t myhash(const HashedObj& x) const {
+        static std::hash<HashedObj> hf;
+        return hf(x) % theLists.size();
     }
 
-    double loadFactor()
-    {
-        // TODO: compute the load factor of hash table, defined on Page 198 of textbook
-        return 0.0;
+
+    double loadFactor(){
+        return static_cast<double>(currentSize) / theLists.size();
     }
 };
 
